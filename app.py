0c079bfb03d1a3ffba3781245174c8fb6ec74534
hoosnick <hoosnick000@gmail.com>
2023-09-07 20:21:10 +0500

    refactor: app.py

--- app.py
+++ app.py
@@ -6,22 +6,22 @@ app = FastAPI(
     title="JollyMax Parser API",
     description="API for parsing **PUBG Mobile** User data",
     contact={
-        'name': 'Github',
-        'url': 'https://github.com/hoosnick/jollymax-parser'
+        "name": "Github",
+        "url":  "https://github.com/hoosnick/jollymax-parser",
     },
     license_info={
         "name": "Apache 2.0",
-        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+        "url":  "https://www.apache.org/licenses/LICENSE-2.0.html",
     },
     docs_url="/",
     redoc_url=None
 )
 
 
-@app.get("/get", tags=['GET'])
+@app.get("/get", tags=["GET"])
 async def fetch_user_data(pubg_id: int):
     async with JollyMaxParser(pubg_id) as pubg_user:
         return {
-            'id': pubg_user.id,
-            'nickName': pubg_user.nick_name
+            "id":       pubg_user.id,
+            "nickName": pubg_user.nick_name
         }
